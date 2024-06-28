4eacdb9780c44f8bb8c135372f64dfa0864b5539
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-06-28 16:49:08 +0200

    introduce decap cms

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <h1>Welcome to Fairdentity</h1>
+    <p>
+      Take a look at our <NuxtLink to="/blog">blog</NuxtLink>!
+    </p>
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+})
+
+const { data: posts } = await useAsyncData('posts', () => 
+  queryContent('/blog').find()
+)
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Blog</h1>
+  <NuxtLink to="/">Back Home</NuxtLink>  
+  <ul>
+    <li v-for="post in posts" :key="post.slug">
+      <nuxt-link :to="`${post._path}`">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.description }}</p>
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const { data: posts } = await useAsyncData('posts', () => 
+  queryContent('/blog').find()
+)
+
+</script>
