870bcd32401374c5da320ec95a73322b636c5243
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-03 18:09:54 +0200

    add close menu icon

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -69,7 +69,10 @@
         @click="showMenu=!showMenu" 
         class="sm:hidden"
       >
-        <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg v-if="showMenu" class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+        <svg v-else class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
         </svg>
       </button>
