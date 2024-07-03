54e07e85cdf377cbead813a13956143953029243
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-03 18:10:39 +0200

    fix hero text format with new fixed header

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="pb-20">
-    <section class="py-16 sm:py-28 px-5 sm:px-16 bg-neutral-100 bg-noise">
+    <section class="pb-16 pt-32 sm:pt-36 sm:pb-20 px-5 sm:px-16 bg-neutral-100 bg-noise">
       <div class="mx-auto max-w-4xl">
         <HeroText />
       </div>
