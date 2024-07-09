a7c95af05fbf31d2c83d69b2679b31eac4efbb2d
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-09 13:31:39 +0200

    update font loading

--- app.vue
+++ app.vue
@@ -18,8 +18,7 @@ useHead({
     { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
     { link: "https://fonts.googleapis.com", rel: "preconnect" },
     { link: "https://fonts.gstatic.com", rel: "preconnect", crossorigin: true },
-    // { link: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap", rel: "stylesheet"},
-    { link: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap", rel: "stylesheet"}
+    { link: "https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap", rel: "stylesheet"},
   ],
 })
 </script>
